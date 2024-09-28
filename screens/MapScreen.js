a991e9bf04c794c9513104d703b9d3c1a3ce1367
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-09-28 16:25:38 +0200

    Map integration

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,29 @@
-import React, {useContext} from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useContext, useState, useRef } from 'react';
+import { View, StyleSheet, PanResponder, Animated } from 'react-native';
 import ThemeProvider, { ThemeContext } from '../context/ThemeContext';
+import Svg, { Path, G } from 'react-native-svg';
+import * as d3 from 'd3-geo';
+import worldMapData from '../assets/custom.geo.json';
 
 const MapScreen = () => {
     const theme = useContext(ThemeContext);
 
+    const projection = d3.geoMercator().scale(100).translate([150, 150]);
+    const pathGenerator = d3.geoPath().projection(projection);
+  
     return (
         <View style={[styles.container, { backgroundColor: theme.background }]}>
+            <Svg height="100%" width="100%" viewBox="0 0 300 300" style={{ backgroundColor: theme.primary }}>
+                {worldMapData.features.map((feature, index) => (
+                    <Path
+                        key={index}
+                        d={pathGenerator(feature)}
+                        fill={theme.background} // Country color
+                        stroke={theme.text} // Border color
+                        strokeWidth={0.1} // Border width
+                    />
+                ))}
+            </Svg>
         </View>
     );
 };
